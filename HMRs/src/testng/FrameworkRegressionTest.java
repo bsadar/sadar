02cccf93eb96f1a26390e93cef25198e7ee88dd1
qaplanet <qaplanet@qaplanet-PC>
2019-12-11 19:15:32 +0530

    test

--- HMRs/src/testng/FrameworkRegressionTest.java
+++ HMRs/src/testng/FrameworkRegressionTest.java
@@ -10,7 +10,7 @@ public class FrameworkRegressionTest {
 	WebDriver bo;
 	
 	   @Test(enabled=true,priority=1,groups="Login") 
-	 
+	 //valid&valid
 	  public void TC2_1() {
 		     System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		     bo=new FirefoxDriver();
@@ -18,34 +18,50 @@ public class FrameworkRegressionTest {
 			bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
 	         bo.findElement(By.name("txtPassword")).sendKeys("lab1");
 	        bo.findElement(By.name("Submit")).click();
-	         Assert.assertEquals(bo.getTitle(), "tOrangeHRM");
+	         Assert.assertEquals(bo.getTitle(), "OrangeHRM");
 	         bo.close();
 	  }
-	   @Test(enabled=true,priority=2,groups="Login")  
-	   public void TC2_2() {
+	   @Test(enabled=true,priority=2,groups="Login") 
+	   //valid&invalid
+	   public void TC2_2() throws InterruptedException {
 		     System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		     bo=new FirefoxDriver();
 			bo.get("http://apps.qaplanet.in/qahrm/login.php");
 			bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
-	         bo.findElement(By.name("txtPassword")).sendKeys("lab1");
-	       bo.findElement(By.xpath("/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[2]/input[1]")).click();
-	         Assert.assertEquals(bo.getTitle(), "OrangeHRM");
+			Thread.sleep(3000);
+	         bo.findElement(By.name("txtPassword")).sendKeys("sadar");
+	         Thread.sleep(3000);
+	         bo.findElement(By.xpath("//input[@name='Submit']")).click();
+	         Assert.assertEquals(bo.findElement(By.xpath("/html/body/form/table[1]/tbody/tr/td[2]/table/tbody/tr[1]/td[2]/table/tbody/tr[5]/td[2]/strong/font")).getText(), "Invalid Login");
 	         bo.close();
 	  }
-	   @Test(enabled=true,priority=3,groups="Login")  
+	   @Test(enabled=true,priority=3,groups="Login") 
+	   //invalid&valid
 	   public void TC2_3() {
-		     System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
+		   System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		     bo=new FirefoxDriver();
 			bo.get("http://apps.qaplanet.in/qahrm/login.php");
-			bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
+			bo.findElement(By.xpath("//td[2]/input")).sendKeys("sadar");
 	         bo.findElement(By.name("txtPassword")).sendKeys("lab1");
-	       bo.findElement(By.xpath("/html[1]/body[1]/form[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[2]/input[1]")).click();
-	         Assert.assertEquals(bo.getTitle(), "OrangeHRM");
+	         bo.findElement(By.xpath("//input[@name='Submit']")).click();
+	         Assert.assertEquals(bo.findElement(By.xpath("//font[contains(text(),'Invalid Login')]")).getText(), "Invalid Login");
+	         bo.close();
+	  }
+	   @Test(enabled=true,priority=4,groups="Login")  
+	   //invalid&valid
+	   public void TC2_4() {
+		   System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
+		     bo=new FirefoxDriver();
+			bo.get("http://apps.qaplanet.in/qahrm/login.php");
+			bo.findElement(By.xpath("//td[2]/input")).sendKeys("");
+	         bo.findElement(By.name("txtPassword")).sendKeys("");
+	         bo.findElement(By.xpath("//input[@name='Submit']")).click();
+	         Assert.assertEquals(bo.switchTo().alert().getText(), "User Name not given!");
+	         bo.switchTo().alert().accept();
 	         bo.close();
 	  }
 	 
-	 
-	  @Test (enabled=true,priority=2,groups="admin",dependsOnMethods =  "TC21" )
+	  @Test (enabled=true,priority=5,groups="admin",dependsOnMethods =  "TC2_1" )
 	   public void TC3() throws InterruptedException {
 		  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		       bo=new FirefoxDriver();
@@ -59,7 +75,7 @@ public class FrameworkRegressionTest {
 		  bo.close();
 	                     }
 	  
-	  @Test (enabled=true,priority=2,groups="admin",dependsOnMethods =  "TC21" )
+	  @Test (enabled=true,priority=6,groups="admin",dependsOnMethods =  "TC2_1" )
 	   public void TC4() {
 		  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		     bo=new FirefoxDriver();
