d337dca11057f9754eb0fde6abfcf6a8299d3949
qaplanet <qaplanet@qaplanet-PC>
2019-12-20 18:09:18 +0530

    this is new file for autit

---
+++ HMRs/src/autoit/AutoitCalc.java
@@ -0,0 +1,46 @@
+/*;open application
+Run("calc.exe")
+;Sleep
+Sleep(1000)
+;Activate window
+WinActivate("TiCalculator]","")
+;click on 5
+ControlClick("Calculator","","Button10")
+
+.........................abstract;open application
+Run("calc.exe")
+;Sleep
+Sleep(1000)
+;Activate window
+WinActivate("[Title:Calculator]","")
+;click on 5 
+ControlClick("[Title:Calculator]","","Button10")
+;Sleep
+Sleep(1000)
+;click on 6
+ControlClick("[Title:Calculator]","","Button15")
+;Sleep
+Sleep(1000)
+;Click on multiplication
+ControlClick("[Title:Calculator]","","Button21")
+;Sleep
+Sleep(1000)
+;Click on 5
+ControlClick("[Title:Calculator]","","Button10")
+;Sleep
+Sleep(1000)
+;Click on =
+ControlClick("[Title:Calculator]","","Button28")
+;Sleep
+Sleep(1000)
+;close application 
+WinClose("[Title:Calculator]","")
+
+
+
+
+*/
+
+
+
+
