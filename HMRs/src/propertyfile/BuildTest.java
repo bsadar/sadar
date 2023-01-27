8312ddaa6083a887532c8e6d55d112e2ac80e005
DML <48423549+bsadar@users.noreply.github.com>
2023-01-27 13:53:43 +0530

    change
    changed

--- HMRs/src/propertyfile/BuildTest.java
+++ HMRs/src/propertyfile/BuildTest.java
@@ -14,12 +14,15 @@ public class BuildTest {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+		//reading property file
 		FileInputStream propertyfile = new FileInputStream(System.getProperty("user.dir")+"/src/property/locatorvalues.properties");
 		
+		//oject  for property class and loading prperty file
 		Properties obj = new Properties(); 
 		obj.load(propertyfile);
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chorme107\\chromedriver.exe");
 		
+		//scripting
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chorme107\\chromedriver.exe");
 		WebDriver bo=new ChromeDriver();
 		bo.get(obj.getProperty("bpath"));
 		bo.findElement(By.xpath(obj.getProperty("uidl"))).sendKeys("sadar");
