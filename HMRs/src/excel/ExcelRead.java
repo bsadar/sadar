aea439014ca378773984009ed350e5f407fbf00d
DML <48423549+bsadar@users.noreply.github.com>
2023-01-27 14:04:47 +0530

    Update ExcelRead.java
    changed

--- HMRs/src/excel/ExcelRead.java
+++ HMRs/src/excel/ExcelRead.java
@@ -13,15 +13,18 @@ public class ExcelRead {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileInputStream file= new FileInputStream ("C:\\Users\\qaplanet\\Desktop\\TestCase.xlsx");
-
-
-		XSSFWorkbook wb=new XSSFWorkbook(file); 
-		XSSFSheet sh = wb.getSheet("Sheet2");
-
-		XSSFRow rw=sh.getRow(4); 
 		
+		//get excel file from folder
+		FileInputStream file= new FileInputStream ("C:\\Users\\qaplanet\\Desktop\\TestCase.xlsx");
+		//create workbook object
+		XSSFWorkbook wb=new XSSFWorkbook(file); 
+		//get sheet in workbook
+		XSSFSheet sh = wb.getSheet("Sheet2");
+                //get row in sheet
+		XSSFRow rw=sh.getRow(4); 
+		//get cell in row
 		XSSFCell cel=rw.getCell(2); 
+		//print value cell vallue
 	   System.out.println(cel);
      
   
