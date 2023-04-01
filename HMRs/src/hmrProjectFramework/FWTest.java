407a2c1e46b5875301436d8558355bdaaedd88ae
DML <48423549+bsadar@users.noreply.github.com>
2023-04-01 19:04:09 +0530

    regression suite

--- HMRs/src/hmrProjectFramework/FWTest.java
+++ HMRs/src/hmrProjectFramework/FWTest.java
@@ -9,49 +9,48 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class FWTest {
-	String hmr_Bildpath="http://apps.qaplanet.in/qahrm/login.php";
-	WebDriver bo;
+	WebDriver br;
+	//build path value
+	String bpath="https://bsadar.github.io/hmrbuild1/Loging.html";
 	
-  @Test(enabled=true,priority=2,groups="Login")
- 
-  public void TC_0002_1() {
-	  bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
-	    bo.findElement(By.name("txtPassword")).sendKeys("lab1");
-	   bo.findElement(By.name("Submit")).click();
-	   Assert.assertEquals(bo.getTitle(), "aOrangeHRM");
-  }
-  
-  @Test(enabled=true,priority=2,groups="Login")
-  
-  public void TC_0002_2() {
-	  bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
-	    bo.findElement(By.name("txtPassword")).sendKeys("invalid");
-	   bo.findElement(By.name("Submit")).click();
-	   Assert.assertEquals(bo.findElement(By.xpath("//font[contains(text(),'Invalid Login')]")).getText(),"Invalid Login");
-	 }
-  
-  @Test(enabled=true,priority=2,groups="admin",dependsOnMethods =  "TC_0002_1")
-  //,dependsOnMethods =  ""
-  public void TC_0004() {
-	  bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
-	    bo.findElement(By.name("txtPassword")).sendKeys("lab1");
-	   bo.findElement(By.name("Submit")).click();
-	   bo.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
-	  Assert.assertEquals(bo.getTitle(),"OrangeHRM");
-	  bo.close();
-	 }
-  
-  
-  @BeforeMethod
+@BeforeMethod
   public void be() {
-	  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
-	     bo=new FirefoxDriver();
-		bo.get(hmr_Bildpath);
+	  // opening browser
+	   System.setProperty("webdriver.gecko.driver","C:\\Users\\lenovo\\git\\rrr\\sadarclass\\src\\brosers\\geckodriver.exe");
+		 br= new FirefoxDriver();
+		
+		//System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chromedriver.exe");
+		//br=new ChromeDriver();
+	  // entering build path
+		 br.get(bpath);
+		 br.manage().window().maximize();
   }
 
   @AfterMethod
   public void bclose() {
-	  bo.close();
+	  //browser close
+	 br.quit();
   }
+	
+  @Test(enabled=true,priority=1,groups="Login")
+  public void TC_00001() {
+	  //enter valid value in userid text field
+	     br.findElement(By.xpath("//input[contains(@name,'id')]")).sendKeys("sadar");
+	     Thread.sleep(3000)	;	
+	  //enter valid value in password text field
+	     br.findElement(By.xpath("//input[@name='pass']")).sendKeys("sadar");
+	  //click on login button
+	     br.findElement(By.xpath("//input[contains(@value,'Login')]")).click();
+	     //actual result: getting title of page
+		String ar=br.getTitle();
+	     //Expected result: giving expected result
+		String er="MainHMR";
+	  // comparaing expected and actual result
+		Assert.assertEquals(er,ar);
+  }
+  
+  
+  
+ 
 
 }
