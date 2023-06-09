b89b94e6b8dd1729fde1a2b0c83440035865028b
DML <48423549+bsadar@users.noreply.github.com>
2023-06-09 19:05:45 +0530

    Update FrameworkSample.java
    Sample frame work

--- HMRs/src/testng/FrameworkSample.java
+++ HMRs/src/testng/FrameworkSample.java
@@ -11,26 +11,26 @@ import org.testng.annotations.Test;
 public class FrameworkSample {
 	WebDriver bo;
 	String buildpath="http://apps.qaplanet.in/hrm/login.php";
-	   @Test(enabled=true,priority=1,groups="Login") 
-	 //valid&valid
-	  public void TC2_1() {
-		     
-			bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
-	         bo.findElement(By.name("txtPassword")).sendKeys("lab1");
-	        bo.findElement(By.name("Submit")).click();
-	         Assert.assertEquals(bo.getTitle(), "OrangeHRM");
-	       
-	  }
-	  
-	  
+	  //browser opening
 	  @BeforeMethod
 	  void buildExecution(){
 		  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
 		     bo=new FirefoxDriver();
 			bo.get(buildpath);
 	  }
+	//browser close
 	  @AfterMethod
 	  void buildClose(){
 		  bo.close(); 
 	  }
+	
+	 //valid test case
+	  @Test(enabled=true,priority=1,groups="Login") 
+	  public void TC2_1() {
+		bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
+	         bo.findElement(By.name("txtPassword")).sendKeys("lab1");
+	        bo.findElement(By.name("Submit")).click();
+	         Assert.assertEquals(bo.getTitle(), "OrangeHRM");     
+	  }
+	  
 }
