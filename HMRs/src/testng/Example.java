28c4e6c6409a7cdaf7529c5f38612ddd1c4823e1
DML <48423549+bsadar@users.noreply.github.com>
2023-06-09 19:31:45 +0530

    Example of testng test case
    Example of testng test case

--- HMRs/src/testng/Example.java
+++ HMRs/src/testng/Example.java
@@ -10,12 +10,23 @@ public class Example {
   @Test
   public void Tc1() {
 	  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
+	  //open firefox browser
 	    WebDriver  bo=new FirefoxDriver();
+	  //enter URL in borwser address
 		bo.get("http://apps.qaplanet.in/qahrm/login.php");
+	  //enter valid value in userid text field
 		bo.findElement(By.xpath("//td[2]/input")).sendKeys("qaplanet1");
+	  //enter valid value in password text field
 	    bo.findElement(By.name("txtPassword")).sendKeys("lab1");
+	  //click on login button
 	   bo.findElement(By.name("Submit")).click();
-	   Assert.assertEquals(bo.getTitle(), "OrangeHRM");
+	  //Getting Actual Result
+	  String ActualResult=bo.getTitle();
+	  //mentioning Expected Result
+	  String ExpectedResult="OrangeHRM";
+	  //matching Actaul &expected results
+	   Assert.assertEquals(ActualResult,ExpectedResult);
+	  //browser close
 	   bo.close();
   }
   
