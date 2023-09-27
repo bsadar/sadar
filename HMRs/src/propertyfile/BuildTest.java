d8913832cf6b09b4d7df4f8661927e78a885dedb
DML <48423549+bsadar@users.noreply.github.com>
2023-09-27 14:57:39 +0530

    Update BuildTest.java
    Update BuildTest.java

--- HMRs/src/propertyfile/BuildTest.java
+++ HMRs/src/propertyfile/BuildTest.java
@@ -15,16 +15,21 @@ public class BuildTest {
 		// TODO Auto-generated method stub
 		
 		                //reading property file
-				FileInputStream propertyfile = new FileInputStream(System.getProperty("user.dir")+"/src/propertyfile/locatorvalues.properties");
+		                FileInputStream propertyfile = new FileInputStream("C:\\Users\\lenovo\\git\\rrr\\sadarclass\\src\\propertyfile\\locatorvalues.properties");
+				//Or
+                                //FileInputStream propertyfile = new FileInputStream(System.getProperty("user.dir")+"/src/propertyfile/locatorvalues.properties");
 				
-				//creating propertyoject  for property class & loading prperty file
+		        	//creating propertyoject  for property class & loading prperty file
 				Properties propertyoject = new Properties(); 
 				propertyoject.load(propertyfile);
 				
-				//scripting with property file variable
-				System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chorme107\\chromedriver.exe");
+				//opening chrome browser
+				System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chorme107\\chromedriver.exe"); 
 				WebDriver bo=new ChromeDriver();
+		
+		                //scripting with property file variable reading
 				bo.get(propertyoject.getProperty("bpath"));
+		                //entering userid text field 
 				bo.findElement(By.xpath(propertyoject.getProperty("uidl"))).sendKeys(propertyoject.getProperty("uidv"));
 	}
 
