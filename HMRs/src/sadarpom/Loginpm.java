e4f01cc8f6452cda6d4c1d768aed67bd62bf9586
DML <48423549+bsadar@users.noreply.github.com>
2025-01-24 08:52:31 +0530

    Update Loginpm.java
    test

--- HMRs/src/sadarpom/Loginpm.java
+++ HMRs/src/sadarpom/Loginpm.java
@@ -9,10 +9,10 @@ public class Loginpm{
     //sadar 22/8/2019
   
    //shareef 23/7/2017
-   By uname=By.xpath("//td[2]/input");
+   By unamelocator=By.xpath("//td[2]/input");
    //method of uuid text field
    public void ue_txt(String unamevalue) {
-	   bo.findElement(uname).sendKeys(unamevalue);
+	   bo.findElement(unamelocator).sendKeys(unamevalue);
 	 	
 	 }
 	//passing value from constrctor to this class variable
