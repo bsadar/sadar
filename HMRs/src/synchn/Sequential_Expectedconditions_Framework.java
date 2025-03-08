5e86d1cf3f71f4f5ff29e6851f8033409cad0e2f
DML <48423549+bsadar@users.noreply.github.com>
2025-03-08 11:41:07 +0530

    Add files via upload
    test

---
+++ HMRs/src/synchn/Sequential_Expectedconditions_Framework.java
@@ -0,0 +1,50 @@
+package synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Sync {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\browserexe\\chorme107\\chromedriver.exe");
+		//open chrome browser
+		ChromeDriver  bo=new ChromeDriver();	
+	    bo.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+	   // enter url
+	    bo.get("https://bsadar.github.io/hmrbuild1/Loging.html");
+	     // wait object
+	    WebDriverWait wait=new WebDriverWait(bo,Duration.ofMillis(3000));
+	    
+	    // verify title of page
+	    if(wait.until(ExpectedConditions.titleIs("HMR"))) {
+	    	System.out.println("page is displayed:pass");
+	    	
+	    }
+	    else {
+	    	System.out.println("page is not displayed:fail");
+	    }
+	    
+	    // 1.verify user id text field
+	    
+	    WebElement ud=   wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
+	    if(ud.isDisplayed()) {
+	    	System.out.println("ud displayed :pass");
+	    }
+	    else {
+	    	System.out.println("ud not displayed :fail");
+	    }
+           //2. after verify ,enter userid value
+	    ud.sendKeys("sadar");
+	    
+	    
+	    
+}
+}
